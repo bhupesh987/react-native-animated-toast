f8675dd3655e4421cb7bc39fba3057288b5866d1
BhuPesh kumar <bhupesh.kumar@owebest.com>
2023-07-11 13:38:27 +0530

    version update

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,6 @@
-declare module 'react-native-custom-toast-view';
+import {ToastProvider as ToastWrapper, useToastProvider as toastContext} from './src/ToastContext';
+
+declare module 'react-native-custom-toast-view'{
+    export const ToastProvider = ToastWrapper;
+    export const useToastProvider = toastContext;
+};
